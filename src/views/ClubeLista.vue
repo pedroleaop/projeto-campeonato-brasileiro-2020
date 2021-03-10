d7056d73086519ca4d1580666d57ddfa7bdc6230
pedroleaop <pedroleaop@hotmail.com>
2021-03-10 18:03:49 -0300

    final

--- src/views/ClubeLista.vue
+++ src/views/ClubeLista.vue
@@ -12,23 +12,23 @@
           <th colspan="2" class="text-left">
             Clubes
           </th>
-          <th class="text-left">
+          <th class="text-right">
             Pontos
           </th>
         </tr>
         
       </thead>
       <tbody>
-        <tr v-for="(clube, index) of clubesLista" :key="clube.id">
+        <tr v-for="(clube, index) of clubesListaOrdem" :key="clube.id">
            <td>{{index+1}}</td>
-           <td> </td>
-          <td><v-avatar>
+          <td><v-avatar size="24">
                 <img
                     :src="clube.escudo"
                     :alt="clube.nome"
                 >
-                </v-avatar>{{ clube.nome  }}</td>
-            <td>{{ clube.pontos  }}</td>
+                </v-avatar> - <span style="padding-left:2%;">{{ clube.nome  }}</span>
+                </td>
+            <td class="text-right">{{ clube.pontos }}</td>
         </tr>
       </tbody>
     </template>
@@ -43,19 +43,22 @@ export default {
     data() {
         return {
             clubesLista: []
-        }
-    },
+        }},
+
     created() {
         fetch('https://hackthon-decola.firebaseio.com/clubes-lista.json')
         .then(resposta => resposta.json())
         .then(json =>
                 this.clubesLista = json)
                 console.log(this.clubesLista)
-            }
-
-}
+            },
+    computed: {
+        clubesListaOrdem() {
+            const listaOrdenada = this.clubesLista.slice(0).sort(
+                (a,b) => a.pontos > b.pontos ? -1 : 1);
+                return listaOrdenada;
+        }
+    }
+    }
+                
 </script>
-
-<style scoped>
-
-</style>
