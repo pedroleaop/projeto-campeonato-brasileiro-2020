5472d1506b601e8818a8fe6b1ea526ffb8c9b5b8
pedroleaop <pedroleaop@hotmail.com>
2021-03-10 16:04:24 -0300

    comits

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,22 @@
 <template>
-
   <v-app>
-  <div>
-    <v-app-bar
-      color="green lighten-1"
-      dense
-      dark
-    > 
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
-      
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="green--text text--lighten-2"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </div>
-  <v-main style="margin-left:3%; margin-top:2%">Teste</v-main>
+    <AppTopBar/>
+  <v-main>
+    <v-container><Inicio/></v-container>
+  </v-main>
   </v-app>
 </template>
 
 <script>
+import Inicio from  './components/Inicio';
+import AppTopBar from './components/shared/AppTopBar';
+
 export default {
   name: 'App',
 
   components: {
+    Inicio,
+    AppTopBar
   },
 
   data: () => ({
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-app-bar
+      color="green lighten-1"
+      dense
+      dark
+    > 
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
+      
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="green--text text--lighten-2"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AppTopBar',
+    data: () => ({
+        drawer: false,
+        group: null,
+        }),
+};
+
+</script>
+
+<style>
+
+</style>
